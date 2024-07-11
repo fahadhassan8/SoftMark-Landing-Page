5d1140b062e247d38c416c1dedd9a9358201bb4d
fahadhassan8 <fahadfuuast2@gmail.com>
2024-07-11 10:39:07 -0700

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
-import Navbars from "./Navbar/Navbar.jsx";
 import Cover from "./Cover/cover.jsx";
 // import Cards from "./components/Cards/Cards";
 import MultipleCard from "./multipleCards/MultipleCard.jsx";
 import ContactUs from "./ContactUs/ContactUs.jsx";
-import Footer from "./Footer/Footer.jsx";
 
 function Home() {
   return (
