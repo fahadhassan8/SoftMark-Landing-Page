ee751b7c7b27496c78a3c18660c9a8fdabd72da1
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-07-16 03:48:46 +0500

    routing

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css';
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./contactUs.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 function ContactUs() {
   useEffect(() => {
@@ -17,23 +17,47 @@ function ContactUs() {
         <h1>Get in Touch</h1>
       </div>
       <div className="form">
-        <div className="form-content">  
+        <div className="form-content">
           <label>
-            <input type="text" placeholder="Name" data-aos="fade-up" data-aos-duration="1500"/>
+            <input
+              type="text"
+              placeholder="Name"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            />
           </label>
           <label>
-            <input type="email" placeholder="Email" data-aos="fade-up" data-aos-duration="1500"/>
+            <input
+              type="email"
+              placeholder="Email"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            />
           </label>
           <label>
-            <input type="number" placeholder="Phone Number"data-aos="fade-up" data-aos-duration="1500" />
+            <input
+              type="number"
+              placeholder="Phone Number"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            />
           </label>
           <label>
-            <input type="text" placeholder="Company" data-aos="fade-up" data-aos-duration="1500"/>
+            <input
+              type="text"
+              placeholder="Company"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            />
           </label>
           <div className="dropdown-container">
-            <select className="dropdown" data-aos="fade-up" data-aos-duration="1500">
-              <option value="option1" disabled>
-                select a service
+            <select
+              className="dropdown"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            >
+              <option value="option1" disabled selected>
+                Select a service
               </option>
               <option value="option2">Web Development</option>
               <option value="option3">Mobile App & Web Development</option>
@@ -41,12 +65,24 @@ function ContactUs() {
               <option value="option5">Digital Marketing</option>
             </select>
           </div>
-
-          <textarea placeholder="Enter your text here..." data-aos="fade-up" data-aos-duration="1500"></textarea>
+          <div className="textarea-div">
+            <textarea
+              placeholder="Enter your text here..."
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            ></textarea>
+          </div>
         </div>
-        <div className="contact-btn" data-aos="fade-up" data-aos-duration="1500">
-          <button> <FontAwesomeIcon icon={faPaperPlane} /> 
-          <span>Send message</span></button>
+        <div
+          className="contact-btn"
+          data-aos="fade-up"
+          data-aos-duration="1500"
+        >
+          <button>
+            {" "}
+            <FontAwesomeIcon icon={faPaperPlane} />
+            <span>Send message</span>
+          </button>
         </div>
       </div>
     </div>
